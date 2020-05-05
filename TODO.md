2d9bebea33f4b911b2bd4671818e9fdbb833781d
david-fong <davidfong19@gmail.com>
2020-05-05 12:27:32 -0700

    update assets + test submission injection

--- TODO.md
+++ TODO.md
@@ -1,4 +1,12 @@
 
 # Things To Do
 
+## High Priority
+
+## Maintenance
+
+- Remove temporary fix for duplicated SVG submission boxes
+
+## Learning
+
 [SVG](https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Adding_vector_graphics_to_the_Web)
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 const SVG_NSPS = "http://www.w3.org/2000/svg";
 const XLINK_NSPS = "http://www.w3.org/1999/xlink";
 
-// TODO if repo moves to organization, update link.
-// Also, would it be better to use the version from the
-// `gh-pages` branch? I think no, but I'm not 100% sure yet.
+// TODO if repo moves to organization, update link. Once we enter
+// production, it may be better to use the `gh-pages` branch here...
 const GITHUB_RAW = "https://raw.githubusercontent.com/david-fong/CovidWithGratitude/dev/";
 
 async function makeRequest(url: string, method: string = "GET"): Promise<XMLHttpRequest> {
@@ -27,51 +26,76 @@ async function makeRequest(url: string, method: string = "GET"): Promise<XMLHttp
  *
  */
 class MainScroll {
-    public readonly hostElem: HTMLElement;
-	private readonly xmlDoc: Promise<HTMLElement>;
-	private readonly submissionRects: Promise<ReadonlyArray<SVGRectElement>>;
+    public  readonly xmlHost: HTMLElement;
+	private readonly svg: Promise<SVGSVGElement>;
+	private readonly slots: Promise<Readonly<{
+		all:   readonly SVGRectElement[];
+		free: SVGRectElement[];
+		taken: SVGRectElement[];
+	}>>;
 
     public constructor() {
-		this.hostElem = document.querySelector(".main-scroll__houses") as HTMLElement;
-		this.xmlDoc = makeRequest(MainScroll.SVG_URL)
-			.then((xhr) => xhr.responseXML!.documentElement!);
-		this.xmlDoc.then((xml) => this.hostElem.appendChild(xml));
+		this.xmlHost = document.querySelector(".main-scroll__content") as HTMLElement;
+		this.svg = makeRequest(MainScroll.SVG_URL).then((xhr) => {
+			return xhr.responseXML!.documentElement!;
+		}) as Promise<SVGSVGElement>;
+		this.svg.then((xml) => this.xmlHost.appendChild(xml));
 
-		this.submissionRects = this.xmlDoc.then((xml) => {
-			type r = SVGRectElement;
-			const boxesLayer = xml.querySelector("#graphic_style") as SVGGElement;
-			return Array.from<r>(boxesLayer.childNodes as NodeListOf<r>);
-		});
-		this.submissionRects.then((rects) => {
-			rects.forEach((rect) => {
-				if (!(rect instanceof SVGRectElement)) {
-					console.log(rect);
-				}
-				console.log(rect.tabIndex);
-				rect.style.color = "black";
+		this.slots = this.svg.then((xml) => {
+			const boxesLayer = xml.getElementById("submission_boxes") as SVGGElement;
+			const __allSlots = Array.from(boxesLayer.children) as SVGRectElement[];
+			const allSlots = Object.freeze(
+				__allSlots.splice(__allSlots.length / 2)
+				// TODO.build ^Remove splice temp-fix if we solve the
+				// duplicating issue from Adobe Illustrator's export.
+				// (The second half are unwanted duplicates of the first half).
+			);
+			return Object.freeze({
+				all:   allSlots,
+				free:  allSlots.slice(),
+				taken: [],
 			});
 		});
 	}
 
-	public async registerSubmission(href: string): Promise<void> {
+	/**
+	 *
+	 * @param imageFilename - Must include the file extension.
+	 */
+	public async registerSubmission(imageFilename: string): Promise<SVGImageElement> {
+		const href = "assets/images/submissions/" + imageFilename;
 		const img = document.createElementNS(SVG_NSPS, "image");
+		img.classList.add("submission");
 		img.setAttributeNS(XLINK_NSPS, "href", href);
 		img.tabIndex = 0; // Allow selection via tabbing and click.
-		await this.submissionRects;
+		img.onclick = (ev) => console.log("click!");
 
-		const box = await this.getEmptyBox();
+		const box = await this.takeEmptyBox();
 		img.setAttribute("preserveAspectRatio", "xMidYMid slice");
-		img.setAttribute("x", box.x.baseVal.toString());
-		img.setAttribute("y", box.y.baseVal.toString());
-		img.setAttribute("height", box.height.baseVal.toString());
-		img.setAttribute("width",  box.width.baseVal.toString());
+		img.setAttribute(	  "x", box.x.baseVal.valueAsString);
+		img.setAttribute(     "y", box.y.baseVal.valueAsString);
+		img.setAttribute("height", box.height.baseVal.valueAsString);
+		img.setAttribute( "width", box.width.baseVal.valueAsString);
 		box.insertAdjacentElement("afterend", img);
+		// It should go after since SVG1 uses xml-tree order to determine
+		// paint-order, and we want it to go _on top_ of the slot rectangle.
+		return img;
 	}
 
-	// TODO.impl also, this should add another copy of the artwork if all
-	// existing submission boxes are full.
-	public async getEmptyBox(): Promise<SVGRectElement> {
-		return undefined!;
+	/**
+	 *
+	 */
+	public async takeEmptyBox(): Promise<SVGRectElement> {
+		const slots = await this.slots;
+		if (slots.free.length) {
+			const take = slots.free.shift()!;
+			slots.taken.push(take);
+			return take;
+		} else {
+			// There are no slots remaining. Extend the artwork:
+			// TODO extend the artwork.
+			return undefined!;
+		}
 	}
 }
 namespace MainScroll {
@@ -79,6 +103,8 @@ namespace MainScroll {
 	// XHR without HTTPS (because it may not be safe from attackers).
 	export const SVG_URL = GITHUB_RAW + "assets/images/houses.svg";
 }
+Object.freeze(MainScroll);
+Object.freeze(MainScroll.prototype);
 
 /**
  * Instantiate it:
